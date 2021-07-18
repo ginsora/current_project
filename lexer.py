d954167bd795543ede824fb2137abda0b80d8c7d
Valeria Kaprilevskaya <kaprilevskaya.valeria@gmail.com>
2021-07-18 16:48:00 +0700

    added tests

--- lexer.py
+++ lexer.py
@@ -5,25 +5,73 @@ TOK_FOR = 104
 TOK_WHILE = 105
 
 
-def word(data):
+class Token:
+
+    def __init__(self, token_id, data):
+        self.token_id = token_id
+        self.data = data
+
+    def __repr__(self):
+        rep = 'Token(' + str(self.token_id) + ',' + self.data + ')'
+        return rep
+
+
+def check(char):
+    return char in '0123456789_abcdefjhijklmnopqrstuvwxyz'
+
+
+def tokenize(data):
     keywords = ['fn', 'let', 'if', 'for', 'while']
     res = []
-    dataList = data.split()
+    data_list = data.split()
+    state = 1
+    res1 = ''
 
-    for char in dataList:
-        if char in keywords and char == 'fn':
-            res.append(TOK_FN)
-        elif char in keywords and char == 'let':
-            res.append(TOK_LET)
-        elif char in keywords and char == 'if':
-            res.append(TOK_IF)
-        elif char in keywords and char == 'for':
-            res.append(TOK_FOR)
-        elif char in keywords and char == 'while':
-            res.append(TOK_WHILE)
+    for key in data_list:
+        if key in keywords and key == 'fn':
+            res.append(Token(TOK_FN, 'fn'))
+        elif key in keywords and key == 'let':
+            res.append(Token(TOK_LET, 'let'))
+        elif key in keywords and key == 'if':
+            res.append(Token(TOK_IF, 'if'))
+        elif key in keywords and key == 'for':
+            res.append(Token(TOK_FOR, 'for'))
+        elif key in keywords and key == 'while':
+            res.append(Token(TOK_WHILE, 'while'))
+        elif key == '==':
+            res.append(Token(3, key))
+        elif key == '>=':
+            res.append(Token(4, key))
+        elif key == '<=':
+            res.append(Token(5, key))
+        elif key == '!=':
+            res.append(Token(6, key))
+
+        elif key != keywords:
+
+            for char in key:
+                if state == 1:
+                    if char in '0123456789':
+                        raise AssertionError('Error')
+                    elif check(char):
+                        res1 += char
+                        state = 2
+                    ''' else:
+                        res1 += char
+                        res.append(Token(2, res1))
+                        state = 1 '''
+                elif state == 2:
+                    if check(char):
+                        res1 += char
+                        state = 2
+                    else:
+                        return res1
+
+            res.append(Token(1, res1))  # !!!!!
+            state = 1
+            res1 = ''
     return res
 
 
 if __name__ == '__main__':
-    print(word('if let for while'))
-    print(word('if let bread while'))
+    print(tokenize('if a = b let data jhc1_1'))
--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,28 +1,28 @@
 import unittest
 
-from lexer import word
+from lexer import tokenize
 
 
 class TestLexer(unittest.TestCase):
 
     def test_work(self):
         data = 'if let for while'
-        result = word(data)
+        result = tokenize(data)
         self.assertEqual([103, 102, 104, 105], result)
 
     def test_upper(self):
         data = 'IF LET FOR WHILE'
-        result = word(data)
+        result = tokenize(data)
         self.assertEqual([], result)
 
     def test_gib(self):
         data = 'jcnlidshoaisc'
-        result = word(data)
+        result = tokenize(data)
         self.assertEqual([], result)
 
     def test_slip(self):
         data = 'if let bread while'
-        result = word(data)
+        result = tokenize(data)
         self.assertEqual([103, 102, 105], result)
 
 
